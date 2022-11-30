5411078b586b2d25b49bb22230a5e9949bbdbd78
Robert Gleason <l2ob122@gmail.com>
2022-11-30 07:38:25 -0800

    finished app with JS, going to refactor to TS support

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
+import TodoTable from "./components/TodoTable";
+import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
+    const [todos, setTodos] = useState([
+        {
+            rowNumber: 1,
+            rowDescription: "Feed Dog",
+            rowAssigned: "Hank"
+        },
+        {
+            rowNumber: 2,
+            rowDescription: "Get in the flask",
+            rowAssigned: "Little Nicky"
+        },
+        {
+            rowNumber: 3,
+            rowDescription: "Learn to code",
+            rowAssigned: "Rob",
+        },
+        {
+            rowNumber: 4,
+            rowDescription: "Buy some steaks",
+            rowAssigned: "Jason",
+
+        }
+    ])
+
+    // this is for our onclick event
+    const addTodo = (description, assigned) => {
+        let rowNumber = 0;
+        if (todos.length > 0) {
+            rowNumber = todos[todos.length - 1].rowNumber + 1;
+        } else {
+            rowNumber = 1;
+        }
+        const newTodo = {
+            rowNumber: todos.length + 1,
+            rowDescription: description,
+            rowAssigned: assigned
+
+        }
+        // this is from our useState hook, this is how we update the state and add the new items
+        setTodos(todos => [...todos, newTodo])
+    }
+
+    // delete todo function
+    const deleteTodo = (deleteTodoRowNumber) => {
+        // this is from our useState hook, this is how we update the state and add the new items
+        let filtered = todos.filter(todo => todo.rowNumber !== deleteTodoRowNumber)
+        setTodos(filtered)
+
+    }
+    return (
+        <div className="mt-5 container">
+            <div className="card">
+                <div className="card-header">
+                    Your Todo's
+                </div>
+                <div className="card-body">
+                    <TodoTable todos={todos} deleteTodo={deleteTodo}/>
+                    <button className="btn btn-primary"
+                            onClick={() => setShowAddTodoForm(!showAddTodoForm)}>{showAddTodoForm ? "Hide" : "Add Todo"}
+                    </button>
+                    {showAddTodoForm &&
+                        <NewTodoForm addTodo={addTodo}/>
+                    }
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
